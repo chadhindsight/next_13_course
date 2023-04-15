cdde27463f7b1ca68f43307c37421b6eb7ec6a7f
chad hinds <chadrickj8@gmail.com>
2023-04-15 12:17:22 -0400

    Make layout

---
+++ app/about/layout.jsx
@@ -0,0 +1,10 @@
+const AboutLayout = ({children}) =>{
+    return (
+        <div>
+            <h1>This is the about layout ting</h1>
+            {children}
+        </div>
+    )
+}
+
+export default AboutLayout
---
+++ app/about/page.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const AboutPage = () => {
+    // NB: Every custom route you make will require a new folder, not a new file
+    return (
+        <div>
+            About Page
+        </div>
+    );
+};
+
+export default AboutPage;
