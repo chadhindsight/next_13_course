cf1458810064512ef5f4717a8a394d99308e9815
chad hinds <chadrickj8@gmail.com>
2023-04-17 13:23:49 -0400

    Add  meta data specific for only the about page

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -1,5 +1,10 @@
 import React from 'react';
 
+// This is meta data specific for only this page 
+export const metadata = {
+    title: 'About Next 13 tutorial'
+}
+
 const AboutPage = () => {
     // NB: Every custom route you make will require a new folder, not a new file
     return (
