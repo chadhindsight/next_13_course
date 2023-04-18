987e9f1e47bbc21f97bca93083a8c4024578525e
chad hinds <chadrickj8@gmail.com>
2023-04-18 13:44:32 -0400

    Get and display the name of currently selected repo

---
+++ app/code/repos/[name]/page.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const RepoPage = ({params}) => {
+    const {name} = params
+
+    return (
+        <div className="card">
+            <h2>{name}</h2>
+            <p>Repo Details</p>
+        </div>
+    );
+};
+
+export default RepoPage;
