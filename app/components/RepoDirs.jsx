45b848c811bf319adc1463da017bbe5a9ef3eef1
chad hinds <chadrickj8@gmail.com>
2023-04-19 11:51:38 -0400

    Fix typo

--- app/components/RepoDirs.jsx
+++ app/components/RepoDirs.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 
 async function fetchRepoContents(name) {
-    
+
 }
 
 const RepoDirs = async ({name}) => {
-    const repContents = await fetchRepoContents(name)
+    const repoContents = await fetchRepoContents(name)
 
     return (
         <div>
