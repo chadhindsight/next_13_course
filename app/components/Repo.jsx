fec003076a876d7d79931a31dc6390492b354b7a
chad hinds <chadrickj8@gmail.com>
2023-04-19 11:39:06 -0400

    Implement the react icons needed

--- app/components/Repo.jsx
+++ app/components/Repo.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { FaCodeBranch, FaStar } from 'react-icons/fa';
 
 const fetchRepo = async function (name) {
     const response = await fetch(`https://api.github.com/repos/chadhindsight/${name}`)
@@ -13,7 +14,22 @@ const Repo = async ({name}) => {
 
     return (
         <div>
-            {repo.name}
+            <h2>{repo.name}</h2>
+            <p>{repo.description}</p>
+            <div className='card-stats'>
+                <div className='card-stat'>
+                    <FaStar />
+                    <span>{repo.stargazers_count}</span>
+                </div>
+                 <div className='card-stat'>
+                    <FaCodeBranch />
+                    <span>{repo.forks_count}</span>
+                </div>
+                 <div className='card-stat'>
+                    <FaEye />
+                    <span>{repo.watchers_count}</span>
+                </div>
+            </div>
         </div>
     );
 };
