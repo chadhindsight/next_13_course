dda1d316aadf67a36dadd251ba8cc5d6798c1213
chad hinds <chadrickj8@gmail.com>
2023-04-19 11:47:01 -0400

    dynamic page route for specific repos

--- app/code/repos/[name]/page.jsx
+++ app/code/repos/[name]/page.jsx
@@ -1,14 +1,21 @@
-import React from 'react';
-import Repo from '@/app/components/Repo';
+import { Suspense } from 'react';
+import Link from 'next/link';
+import Repo from '@/app/componets/Repo';
+import RepoDirs from '@/app/componets/RepoDirs';
 
-const RepoPage = ({params}) => {
-    const {name} = params
-
-    return (
-        <div className="card">
-           <Repo name={name}/>
-        </div>
-    );
+const RepoPage = ({ params: { name } }) => {
+  return (
+    <div className='card'>
+      <Link href='/code/repos' className='btn btn-back'>
+        Back To Repositories
+      </Link>
+      <Suspense fallback={<div>Loading repo...</div>}>
+        <Repo name={name} />
+      </Suspense>
+      <Suspense fallback={<div>Loading directories...</div>}>
+        <RepoDirs name={name} />
+      </Suspense>
+    </div>
+  );
 };
-
 export default RepoPage;
