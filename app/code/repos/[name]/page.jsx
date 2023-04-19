a0be9943d6f7e54f996078c5d065b52188e00c39
chad hinds <chadrickj8@gmail.com>
2023-04-19 12:22:03 -0400

    Change loading message

--- app/code/repos/[name]/page.jsx
+++ app/code/repos/[name]/page.jsx
@@ -9,7 +9,7 @@ const RepoPage = ({ params: { name } }) => {
       <Link href='/code/repos' className='btn btn-back'>
         Back To Repositories
       </Link>
-      <Suspense fallback={<div>Loading repo...</div>}>
+      <Suspense fallback={<div>Loading current repo...</div>}>
         <Repo name={name} />
       </Suspense>
       <Suspense fallback={<div>Loading directories...</div>}>
