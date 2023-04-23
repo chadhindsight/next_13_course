81066e5975391b48eb285fd2a33d6b530fce066a
chad hinds <chadrickj8@gmail.com>
2023-04-23 11:45:53 -0400

    Add comment about route handler

--- app/api/courses/route.js
+++ app/api/courses/route.js
@@ -1,6 +1,7 @@
 import courses from './data.json';
 import { NextResponse } from 'next/server';
 
+// NB: you must name the function after the name of the method being used. example, GET, PUT, etc.
 export async function GET() {
     return NextResponse.json(courses)
 }
