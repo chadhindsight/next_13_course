41d1fa3dca8cb0fdf98aa3868684768125240306
chad hinds <chadrickj8@gmail.com>
2023-04-24 12:11:56 -0400

    Add Course Search

--- app/page.jsx
+++ app/page.jsx
@@ -1,14 +1,35 @@
 'use client';
-import Link from "next/link";
-import Courses from "./components/Courses";
+import { useState, useEffect } from 'react';
+// import Link from "next/link";
+import LoadingPage from './loading';
+import Courses from './components/Courses';
+import CourseSearch from './components/CourseSearch';
 
 const HomePage = () => {
+  const [courses, setCourses] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchCourses = async () => {
+      const res = await fetch('/api/courses');
+      const data = await res.json();
+      setCourses(data);
+      setLoading(false);
+    };
+
+    fetchCourses();
+  }, []);
+
+  if (loading) {
+    return <LoadingPage />;
+  }
+
   return (
     <>
-      <h1>Welcome to Chad's Next App</h1>
-     <Courses />
-     </>
+      <h1>Welcome To Traversy Meida</h1>
+      <CourseSearch getSearchResults={(results) => setCourses(results)} />
+      <Courses courses={courses} />
+    </>
   );
 };
-
 export default HomePage;
